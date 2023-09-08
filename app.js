f83e5312a09b9d17f7ff5528d5594b40e8f478d9
cyaomasa <cyao.masa.0924@gmail.com>
2023-09-08 23:59:08 +0000

    practice-2

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('Hello World');
+let hello = "Hello World";
+alert(hello);
+
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World';
+
+alert(str1 + str2);
+
+// 四則演算
+let num1 = 100;
+let num2 = 200;
+
+alert(num2 + num1);
+alert(num2 - num1);
+alert(num2 * num1);
+alert(num2 / num1);
